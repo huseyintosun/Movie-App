fdb2e5838a3b2769e4c2adcc6eac841900d9045b
huseyintosun <huseyin.tosun.8818@gmail.com>
2021-09-09 23:08:19 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import AuthContextProvider from './context/AuthContext';
+import AppRouter from './router/Router'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <AuthContextProvider>
+      <AppRouter/>
+    </AuthContextProvider>
   );
 }
 
